279867760ef73b4337f389dde4af6b3d257c5662
Felipe Polato <fepolato@hotmail.com>
2021-09-17 16:48:24 -0300

    rotas com get e post e database

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -4,5 +4,6 @@ const userController = require("../controller/user");
 const userRouter = new express.Router();
 
 userRouter.post("/", userController.postUser);
+userRouter.get("/", userController.gettUser);
 
 module.exports = userRouter
