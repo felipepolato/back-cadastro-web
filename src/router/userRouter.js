e292635c33407cd94c608078b43c96c2af1acb9e
Felipe Polato <fepolato@hotmail.com>
2021-09-26 12:32:17 -0300

    rotas para gerar token

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -3,7 +3,8 @@ const userController = require("../controller/user");
 
 const userRouter = new express.Router();
 
-userRouter.post("/", userController.postUser);
 userRouter.get("/", userController.getUser);
+userRouter.post("/", userController.postUser);
+userRouter.post("/authenticate", userController.postAuth);
 
-module.exports = userRouter
+module.exports = userRouter;
