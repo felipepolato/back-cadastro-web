7616f03a1dffe975d0456a3015f00f69b74cd2d9
Felipe <fepolato@hotmail.com>
2021-09-28 21:34:38 -0300

    autenticaçao com jwt

---
+++ src/controller/auth.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+require("../models/user");
+const User = mongoose.model("User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+//POST /authenticate
+module.exports.postAuth = async (req, res, next) => {
+  try {
+    const { email, password, cpf } = req.body;
+
+    if (!email || !password || !cpf) {
+      throw new Error("Parâmetro Inexistente!");
+    }
+
+    const query = {
+      cpf: cpf,
+    };
+
+    if (email.includes("@")) {
+      query.email = email;
+    } else query.pis = email;
+
+    const user = await User.findOne(query).lean();
+
+    if (!user) {
+      throw new Error("Usuário não encontrado!");
+    }
+    console.log(password, user);
+    if (!(await bcrypt.compare(password, user.password))) {
+      throw new Error("Senha invalida!");
+    }
+
+    const responseUser = {
+      ...user,
+    };
+
+    delete responseUser.password;
+
+    const token = jwt.sign({ user: responseUser }, "senhasecreta", {
+      subject: user._id.toString(),
+      expiresIn: "1d",
+    });
+
+    return res.json({ token, user: responseUser });
+
+    ///TODO Tratar Erro ////////
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error: error.message });
+  }
+};
