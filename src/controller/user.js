1a3103bb70149837c6d43cc7b616bdc3e9599e0d
Felipe <fepolato@hotmail.com>
2021-09-29 23:37:48 -0300

    alterar senha usuario

--- src/controller/user.js
+++ src/controller/user.js
@@ -21,20 +21,6 @@ module.exports.postUser = async (req, res, next) => {
       password,
     } = req.body;
 
-    console.log(
-      name,
-      email,
-      country,
-      state,
-      city,
-      street,
-      number,
-      cep,
-      cpf,
-      pis,
-      password
-    );
-
     if (
       !name ||
       !email ||
@@ -68,8 +54,38 @@ module.exports.postUser = async (req, res, next) => {
     const user = await User.create(newUser);
 
     return res.json(user);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+};
 
-    ///TODO Tratar Erro ////////
+/////PUT /user/
+module.exports.putUser = async (req, res, next) => {
+  try {
+    const { password, newPassword } = req.body;
+
+    if (!password || !newPassword) {
+      throw new Error("Parâmetro Inexitente!");
+    }
+
+    const user = await User.findOne({
+      _id: req.userId,
+    });
+
+    if (!user) {
+      throw new Error("Token com usuário inexistente.");
+    }
+
+    const verifiedPassword = await bcrypt.compare(password, user.password);
+    if (!verifiedPassword) {
+      console.log("AQUIIII");
+      throw new Error("Senha Incorreta!");
+    }
+
+    user.password = newPassword;
+    await user.save();
+
+    res.status(200).send();
   } catch (error) {
     return res.status(400).json({ error: error.message });
   }
@@ -78,9 +94,9 @@ module.exports.postUser = async (req, res, next) => {
 ///DELETE  /user/
 module.exports.deleteUser = async (req, res, next) => {
   try {
-    const userId = req.userId
-    User.findByIdAndDelete(userId)
-    res.json("Usuario Deletado")
+    const userId = req.userId;
+    await User.findByIdAndDelete(userId);
+    res.json("Usuario Deletado");
   } catch (error) {
     return res.status(400).json({ error: error.message });
   }
